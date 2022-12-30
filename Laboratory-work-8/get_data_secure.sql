b7a798b050dd83821077797ffa8e17e3ff7ea3d3
OlkaLyubomska <46083045+OlkaLyubomska@users.noreply.github.com>
2022-12-30 13:30:07 +0200

    Create get_data_secure.sql

---
+++ Laboratory-work-8/get_data_secure.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE FUNCTION get_data(u_name VARCHAR)
+RETURNS TABLE (o_id INTEGER, name VARCHAR, size INTEGER) AS $$
+DECLARE
+str VARCHAR;
+BEGIN
+str := 'SELECT o_id, name, size FROM office WHERE name = $1';
+RAISE NOTICE 'QUERY=%', str;
+RETURN QUERY EXECUTE str USING u_name;
+END;
+$$ LANGUAGE PLPGSQL;
