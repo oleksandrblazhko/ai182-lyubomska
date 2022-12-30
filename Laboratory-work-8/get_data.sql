44a4a28e089c2ef45a52bff752af0364ae0c2ad7
OlkaLyubomska <46083045+OlkaLyubomska@users.noreply.github.com>
2022-12-30 13:26:50 +0200

    Update get_data.sql

--- Laboratory-work-8/get_data.sql
+++ Laboratory-work-8/get_data.sql
@@ -1,9 +1,9 @@
-CREATE OR REPLACE FUNCTION get_data(u_size INTEGER)
+CREATE OR REPLACE FUNCTION get_data(u_name VARCHAR)
 RETURNS TABLE (o_id INTEGER, name VARCHAR, size INTEGER) AS $$
 DECLARE
 str VARCHAR;
 BEGIN
-str := 'SELECT o_id, name, size FROM office WHERE size = ''' || u_size || '''';
+str := 'SELECT o_id, name, size FROM office WHERE name = ''' || u_name || '''';
 RAISE NOTICE 'QUERY=%', str;
 RETURN QUERY EXECUTE str;
 END;
